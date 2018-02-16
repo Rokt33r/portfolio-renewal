2946f4f301cb4e0e30d225dd11b8c618aaa6d71c
Jiseung Roh <jiseung.roh@gmail.com>
2018-02-16 18:35:46 +0900

    Update contents

--- pages/about.js
+++ pages/about.js
@@ -7,7 +7,7 @@ const About = () => (
     <section className='section'>
       <h1 className='sr-only'>About</h1>
       <p>
-        Hello, I’m Woohyeon Roh, a UI/UX developer based in South Korea. For past 2+ years, I've worked as a both designer and frontend developer with a UX driven background. Typography passionate, clean and pure code inpired, I bridge the gap between design and development. I am able to quickly adapt to new challenges. Being quite hands-on, I enjoy getting involved in the whole process, starting from ideation and conceptualizing to designing, prototyping, and building the product. I also love learning new stuffs and endeavor to quickly follow latest trend of technology.
+        Hello, I’m Woohyeon Roh, a UI/UX developer based in South Korea. For past 3+ years, I've worked as a both designer and frontend developer with a UX driven background. Typography passionate, clean and pure code pursuing, I bridge the gap between design and development. I am able to quickly adapt to new challenges. Being quite hands-on, I enjoy getting involved in the whole process, starting from ideation and conceptualizing to designing, prototyping, and building the product. I also love learning new stuffs and endeavor to quickly follow latest trend of technology.
       </p>
     </section>
   </AppLayout>
